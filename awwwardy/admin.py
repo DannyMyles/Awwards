a237bf23c97757b0af1b2b93c1681c0c77235e1a
DannyMyles <dancun.wawire@student.moringaschool.com>
2021-12-12 23:28:38 +0300

    edited adminl

--- awwwardy/admin.py
+++ awwwardy/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import Projects, Profile, Review
+# from django.contrib import admin
+# from .models import Projects, Profile, Review
 
-# Register your models here.
-admin.site.register(Projects)
-admin.site.register(Profile)
-admin.site.register(Review)
+# # Register your models here.
+# admin.site.register(Projects)
+# admin.site.register(Profile)
+# admin.site.register(Review)
     
